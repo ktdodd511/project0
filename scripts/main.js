1ba60fd146973405a10852535557686995b59678
ktdodd511 <katiedodd@Katies-MacBook-Air.local>
2016-11-06 12:18:13 -0800

    can determine winner, added start button, but text gets put in it for some reason

--- scripts/main.js
+++ scripts/main.js
@@ -2,21 +2,75 @@ $(document).ready(function() {
 
   console.log("js is connected, wooo!!")
 
-  $( document ).on('keyup', function(e) {
-      switch (e.keyCode) {
-          
-          case 77: 
-            $( ".block1" ).animate({ "left": "+=20px" }, "fast" );
-            break;     
-          case 87:
-            $( ".block2" ).animate({ "left": "+=20px" }, "fast" );
-            break;
-        };
 
+  var player1;
+  var player2;
+  var startRace;
+  var mCount = 0;
+  var wCount = 0;
+
+
+  function readyToPlay() {
+    
+    player1 = prompt("Enter player one's name!");
+    player2 = prompt("Enter player two's name!");
+    startRace = prompt("ARE YOU BOTH READY?");
+
+    if (startRace.toLowerCase() === 'y' || startRace.toLowerCase() === 'yes') {
+    start();
+    
+    }
+
+   }; 
+
+
+
+  function start() {
+    alert("GO!!!");
+    
+    $( document ).on('keyup', function(event) {
+        switch (event.keyCode) {
+            
+            case 77: 
+              $( ".block1" ).animate({ "left": "+=20px" }, "fast" );  
+              mCount+=1;
+              winner();  
+              break;
+
+            case 87:
+              $( ".block2" ).animate({ "left": "+=20px" }, "fast" );
+              wCount+=1;
+              winner();
+              break;
+          };
+
+
+    });
+}
+
+  function winner() {
+    if (mCount >= 90) {
+      alert(player1 + " " + "has won!");
+      location.reload();
+
+    } else if (wCount >= 90) {
+      alert(player2 + " " + "has won!");
+      location.reload();
+    } else if (mCount === 90 && wCount === 90) {
+      alert("It's a tie!");
+      location.reload();
+    }
+
+  }
+
+
+$('#start').click (function() {
+  readyToPlay();
+});
+
+$('#reset').click (function() {
+    location.reload();
   });
-
-
-
 
 
 // maybe put the 'cars' in the divs here
